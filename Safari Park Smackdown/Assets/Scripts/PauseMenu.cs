12bfa0f81cbb81bf6490eebe7694e20c32fbcf69
Dylan Foster <magefeus@gmail.com>
2023-10-08 19:10:38 -0400

    added pause menu and new background

--- Safari Park Smackdown/Assets/Scripts/PauseMenu.cs
+++ Safari Park Smackdown/Assets/Scripts/PauseMenu.cs
@@ -5,8 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject _pauseMenu;
+    [SerializeField] private GameObject _pauseButton;
     public void PlayMainMenu()
     {
         SceneManager.LoadScene("Main Menu");
+    }
+
+    public void PauseButton()
+    {
+       Time.timeScale = 0f;
+    }
+    public void ResumeButton()
+    {
+        Time.timeScale = 1.0f;
     }
 }
